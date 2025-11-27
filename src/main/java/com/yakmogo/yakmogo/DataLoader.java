cc8f4b1f26f5bf936b4909795242eae0941ef4d6
dlfjsld1 <ltk900216@gmail.com>
2025-11-27 16:48:30 +0900

    보안 설정(@Value 적용) 및 에러 메시지 설정 변경

--- src/main/java/com/yakmogo/yakmogo/DataLoader.java
+++ src/main/java/com/yakmogo/yakmogo/DataLoader.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.time.LocalTime;
 
 @Component
 @RequiredArgsConstructor
+@Profile("dev")
 public class DataLoader implements CommandLineRunner {
 
 	private final UserRepository userRepository;
