57ca425347b8b4551476cee8355475f6ec5ed390
dlfjsld1 <ltk900216@gmail.com>
2026-01-26 17:57:03 +0900

    feat: Interceptor로 간단한 관리자 인증 및 보안 설정 구현

---
+++ src/main/java/com/yakmogo/yakmogo/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.yakmogo.yakmogo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.yakmogo.yakmogo.interceptor.AdminInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+	private final AdminInterceptor adminInterceptor;
+
+	//인터셉터 등록
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(adminInterceptor)
+			.addPathPatterns("/api/**")
+			.excludePathPatterns("/api/public/**");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:5173") // 프론트 주소
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
+}
---
+++ src/main/java/com/yakmogo/yakmogo/interceptor/AdminInterceptor.java
@@ -0,0 +1,37 @@
+package com.yakmogo.yakmogo.interceptor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AdminInterceptor implements HandlerInterceptor {
+
+	@Value("${admin.password}")
+	private String adminPassword;
+
+	@Override
+	public boolean preHandle(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		Object handler
+	) throws Exception {
+		if (request.getMethod().equals("OPTIONS")) {
+			return true;
+		}
+		//헤더에서 'x-admin-password'를 꺼냄
+		String requestPassword = request.getHeader("x-admin-password");
+
+		//비밀번호 확인
+		if (adminPassword.equals(requestPassword)) {
+			return true;
+		}
+
+		//틀리면 401에러
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "관리자 암호가 필요합니다.");
+		return false;
+	}
+}
