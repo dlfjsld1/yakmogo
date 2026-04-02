4ef706c71ff6f878d745db026730ef9d6848d97c
dlfjsld1 <ltk900216@gmail.com>
2026-04-02 16:54:40 +0900

    refactor findByGuardians_ChatId to return List for multiple users

--- src/main/java/com/yakmogo/yakmogo/repository/UserRepository.java
+++ src/main/java/com/yakmogo/yakmogo/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.yakmogo.yakmogo.repository;
 
 import com.yakmogo.yakmogo.domain.User;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -13,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	// 유저를 찾을 때 'guardians' 리스트도 Eager로 가져오기
 	@Query("SELECT u FROM User u LEFT JOIN FETCH u.guardians WHERE u.id = :id")
 	Optional<User> findByIdWithGuardians(@Param("id") Long id);
+
+	// 한 보호자가 여러 명을 관리할 수 있으므로 Optional 대신 List로 변경
+	List<User> findByGuardians_ChatId(String chatId);
 }
