63e88f5f9a1fba120d12191a094088219d3dd625
dlfjsld1 <ltk900216@gmail.com>
2026-04-06 13:54:58 +0900

    implement SPA forwarding controller for React Router

--- src/main/java/com/yakmogo/yakmogo/controller/SpaController.java
+++ src/main/java/com/yakmogo/yakmogo/controller/SpaController.java
@@ -1,4 +1,16 @@
 package com.yakmogo.yakmogo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SpaController {
+	// . (점)이 포함되지 않은 모든 경로(확장자가 없는 경로)를 index.html로 포워딩
+	@GetMapping(value = {
+		"/{path:[^\\.]*}",
+		"/**/{path:[^\\.]*}"
+	})
+	public String forward() {
+		return "forward:/index.html";
+	}
 }
